90b7e37b6f7e60942661cfc5770f592852e27337
fuwiak <poczta130@gmail.com>
2024-05-17 20:02:54 +0200

    status bar grid search

--- pages/regression_classification.py
+++ pages/regression_classification.py
@@ -2,18 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransformer
+from sklearn.model_selection import train_test_split, GridSearchCV, \
+    RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, \
+    FunctionTransformer
 from sklearn.compose import ColumnTransformer, make_column_selector
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import Ridge, Lasso
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, \
+    AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.impute import SimpleImputer
 from xgboost import XGBRegressor
 import plotly.express as px
 import plotly.graph_objects as go
 from category_encoders import TargetEncoder
+from time import time
 
 # Set page config
 st.set_page_config(page_title='Regression Analysis System', layout='wide')
@@ -24,12 +28,16 @@ st.markdown('''
 This application predicts prices using various regression models. Upload your dataset or use the preloaded data for demonstration.
 ''')
 
-# Load and preprocess data
-@st.cache
-def load_data(filename):
-    return pd.read_csv(filename).select_dtypes(exclude=['datetime', 'datetimetz'])  # Exclude date columns
 
-uploaded_file = st.sidebar.file_uploader("Choose a file, or leave blank to use default data")
+# Load and preprocess data
+@st.cache_data
+def load_data(filename):
+    return pd.read_csv(filename).select_dtypes(
+        exclude=['datetime', 'datetimetz'])  # Exclude date columns
+
+
+uploaded_file = st.sidebar.file_uploader(
+    "Choose a file, or leave blank to use default data")
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
 else:
@@ -37,9 +45,15 @@ else:
 
 # Sidebar - Model setup
 st.sidebar.header("Model Setup")
-target = st.sidebar.selectbox('Select the target variable for prediction:', df.columns, index=df.columns.get_loc('price_usd') if 'price_usd' in df.columns else 0)
-features = st.sidebar.multiselect('Select features to use:', df.columns.drop(target), default=df.columns.drop(target).tolist())
-encode_categorical = st.sidebar.radio("Encode categorical variables:", ["Frequency Encoding", "Target encoding"], index=0)
+target = st.sidebar.selectbox('Select the target variable for prediction:',
+                              df.columns, index=df.columns.get_loc(
+        'price_usd') if 'price_usd' in df.columns else 0)
+features = st.sidebar.multiselect('Select features to use:',
+                                  df.columns.drop(target),
+                                  default=df.columns.drop(target).tolist())
+encode_categorical = st.sidebar.radio("Encode categorical variables:",
+                                      ["Frequency Encoding",
+                                       "Target encoding"], index=0)
 
 model_type = st.sidebar.selectbox('Choose a regression model:', [
     'Ridge', 'Lasso', 'Random Forest Regressor',
@@ -48,11 +62,14 @@ model_type = st.sidebar.selectbox('Choose a regression model:', [
 ])
 
 # Hyperparameters Sidebar
-alpha_ridge, solver_ridge, max_iter_ridge, alpha_lasso, max_iter_lasso, n_estimators_rf, max_depth_rf, min_samples_split_rf, min_samples_leaf_rf, max_depth_dt, min_samples_split_dt, min_samples_leaf_dt, learning_rate_xgb, n_estimators_xgb, max_depth_xgb, subsample_xgb, colsample_bytree_xgb, n_estimators_ada, learning_rate_ada, loss_ada, n_estimators_gb, learning_rate_gb, max_depth_gb, min_samples_split_gb, min_samples_leaf_gb = [None]*25
+alpha_ridge, solver_ridge, max_iter_ridge, alpha_lasso, max_iter_lasso, n_estimators_rf, max_depth_rf, min_samples_split_rf, min_samples_leaf_rf, max_depth_dt, min_samples_split_dt, min_samples_leaf_dt, learning_rate_xgb, n_estimators_xgb, max_depth_xgb, subsample_xgb, colsample_bytree_xgb, n_estimators_ada, learning_rate_ada, loss_ada, n_estimators_gb, learning_rate_gb, max_depth_gb, min_samples_split_gb, min_samples_leaf_gb = [
+                                                                                                                                                                                                                                                                                                                                                                                                                                                    None] * 25
 
 if model_type == 'Ridge':
     alpha_ridge = st.sidebar.slider('Alpha', 0.01, 10.0, 1.0)
-    solver_ridge = st.sidebar.selectbox('Solver', ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga'])
+    solver_ridge = st.sidebar.selectbox('Solver',
+                                        ['auto', 'svd', 'cholesky', 'lsqr',
+                                         'sparse_cg', 'sag', 'saga'])
     max_iter_ridge = st.sidebar.slider('Max Iterations', 100, 10000, 1000)
 elif model_type == 'Lasso':
     alpha_lasso = st.sidebar.slider('Alpha', 0.01, 10.0, 1.0)
@@ -71,11 +88,13 @@ elif model_type == 'XGBoost Regressor':
     n_estimators_xgb = st.sidebar.slider('Number of Estimators', 100, 500, 100)
     max_depth_xgb = st.sidebar.slider('Max Depth', 1, 10, 3)
     subsample_xgb = st.sidebar.slider('Subsample', 0.5, 1.0, 0.8)
-    colsample_bytree_xgb = st.sidebar.slider('Colsample by Tree', 0.5, 1.0, 0.8)
+    colsample_bytree_xgb = st.sidebar.slider('Colsample by Tree', 0.5, 1.0,
+                                             0.8)
 elif model_type == 'AdaBoost Regressor':
     n_estimators_ada = st.sidebar.slider('Number of Estimators', 50, 200, 50)
     learning_rate_ada = st.sidebar.slider('Learning Rate', 0.01, 1.0, 0.1)
-    loss_ada = st.sidebar.selectbox('Loss Function', ['linear', 'square', 'exponential'])
+    loss_ada = st.sidebar.selectbox('Loss Function',
+                                    ['linear', 'square', 'exponential'])
 elif model_type == 'Gradient Boosting Regressor':
     n_estimators_gb = st.sidebar.slider('Number of Estimators', 100, 500, 100)
     learning_rate_gb = st.sidebar.slider('Learning Rate', 0.01, 0.5, 0.1)
@@ -83,15 +102,27 @@ elif model_type == 'Gradient Boosting Regressor':
     min_samples_split_gb = st.sidebar.slider('Min Samples Split', 2, 10, 2)
     min_samples_leaf_gb = st.sidebar.slider('Min Samples Leaf', 1, 5, 1)
 
+# Hyperparameter tuning
+st.sidebar.header("Hyperparameter Tuning")
+search_type = st.sidebar.radio("Search Type",
+                               ["None", "RandomizedSearch", "GridSearch"],
+                               index=0)
+n_iter_search = st.sidebar.number_input(
+    "Number of iterations (for RandomizedSearch)", min_value=1, value=10)
+cv_folds = st.sidebar.number_input("Number of CV folds", min_value=2, value=3)
+
 # Preprocessing configuration
 numeric_cols = make_column_selector(dtype_include=np.number)
 categorical_cols = make_column_selector(dtype_include=object)
 numerical_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median')),
     ('scaler', StandardScaler())])
-categorical_transformer = TargetEncoder() if encode_categorical == "Target encoding" else Pipeline([
-    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
-    ('freq_enc', FunctionTransformer(lambda X: pd.DataFrame(X).apply(lambda x: x.map(x.value_counts(normalize=True)), axis=0).values, validate=False))])
+categorical_transformer = TargetEncoder() if encode_categorical == "Target encoding" else Pipeline(
+    [
+        ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+        ('freq_enc', FunctionTransformer(lambda X: pd.DataFrame(X).apply(
+            lambda x: x.map(x.value_counts(normalize=True)), axis=0).values,
+                                         validate=False))])
 preprocessor = ColumnTransformer(transformers=[
     ('num', numerical_transformer, numeric_cols),
     ('cat', categorical_transformer, categorical_cols)
@@ -99,13 +130,59 @@ preprocessor = ColumnTransformer(transformers=[
 
 # Model Initialization with Hyperparameters
 model_dict = {
-    'Ridge': Ridge(alpha=alpha_ridge, solver=solver_ridge, max_iter=max_iter_ridge) if model_type == 'Ridge' else Ridge(),
-    'Lasso': Lasso(alpha=alpha_lasso, max_iter=max_iter_lasso) if model_type == 'Lasso' else Lasso(),
-    'Random Forest Regressor': RandomForestRegressor(n_estimators=n_estimators_rf, max_depth=max_depth_rf, min_samples_split=min_samples_split_rf, min_samples_leaf=min_samples_leaf_rf) if model_type == 'Random Forest Regressor' else RandomForestRegressor(),
-    'Decision Tree Regressor': DecisionTreeRegressor(max_depth=max_depth_dt, min_samples_split=min_samples_split_dt, min_samples_leaf=min_samples_leaf_dt) if model_type == 'Decision Tree Regressor' else DecisionTreeRegressor(),
-    'XGBoost Regressor': XGBRegressor(learning_rate=learning_rate_xgb, n_estimators=n_estimators_xgb, max_depth=max_depth_xgb, subsample=subsample_xgb, colsample_bytree=colsample_bytree_xgb) if model_type == 'XGBoost Regressor' else XGBRegressor(),
-    'AdaBoost Regressor': AdaBoostRegressor(n_estimators=n_estimators_ada, learning_rate=learning_rate_ada, loss=loss_ada) if model_type == 'AdaBoost Regressor' else AdaBoostRegressor(),
-    'Gradient Boosting Regressor': GradientBoostingRegressor(n_estimators=n_estimators_gb, learning_rate=learning_rate_gb, max_depth=max_depth_gb, min_samples_split=min_samples_split_gb, min_samples_leaf=min_samples_leaf_gb) if model_type == 'Gradient Boosting Regressor' else GradientBoostingRegressor()
+    'Ridge': Ridge(alpha=alpha_ridge, solver=solver_ridge,
+                   max_iter=max_iter_ridge) if model_type == 'Ridge' else Ridge(),
+    'Lasso': Lasso(alpha=alpha_lasso,
+                   max_iter=max_iter_lasso) if model_type == 'Lasso' else Lasso(),
+    'Random Forest Regressor': RandomForestRegressor(
+        n_estimators=n_estimators_rf, max_depth=max_depth_rf,
+        min_samples_split=min_samples_split_rf,
+        min_samples_leaf=min_samples_leaf_rf) if model_type == 'Random Forest Regressor' else RandomForestRegressor(),
+    'Decision Tree Regressor': DecisionTreeRegressor(max_depth=max_depth_dt,
+                                                     min_samples_split=min_samples_split_dt,
+                                                     min_samples_leaf=min_samples_leaf_dt) if model_type == 'Decision Tree Regressor' else DecisionTreeRegressor(),
+    'XGBoost Regressor': XGBRegressor(learning_rate=learning_rate_xgb,
+                                      n_estimators=n_estimators_xgb,
+                                      max_depth=max_depth_xgb,
+                                      subsample=subsample_xgb,
+                                      colsample_bytree=colsample_bytree_xgb) if model_type == 'XGBoost Regressor' else XGBRegressor(),
+    'AdaBoost Regressor': AdaBoostRegressor(n_estimators=n_estimators_ada,
+                                            learning_rate=learning_rate_ada,
+                                            loss=loss_ada) if model_type == 'AdaBoost Regressor' else AdaBoostRegressor(),
+    'Gradient Boosting Regressor': GradientBoostingRegressor(
+        n_estimators=n_estimators_gb, learning_rate=learning_rate_gb,
+        max_depth=max_depth_gb, min_samples_split=min_samples_split_gb,
+        min_samples_leaf=min_samples_leaf_gb) if model_type == 'Gradient Boosting Regressor' else GradientBoostingRegressor()
+}
+
+param_distributions = {
+    'Ridge': {'model__alpha': np.linspace(0.01, 10.0, 100),
+              'model__solver': ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg',
+                                'sag', 'saga']},
+    'Lasso': {'model__alpha': np.linspace(0.01, 10.0, 100),
+              'model__max_iter': [100, 500, 1000, 5000, 10000]},
+    'Random Forest Regressor': {
+        'model__n_estimators': [100, 200, 300, 400, 500],
+        'model__max_depth': [None, 10, 20, 30],
+        'model__min_samples_split': [2, 5, 10],
+        'model__min_samples_leaf': [1, 2, 4]},
+    'Decision Tree Regressor': {'model__max_depth': [None, 10, 20, 30],
+                                'model__min_samples_split': [2, 5, 10],
+                                'model__min_samples_leaf': [1, 2, 4]},
+    'XGBoost Regressor': {'model__n_estimators': [100, 200, 300, 400, 500],
+                          'model__learning_rate': [0.01, 0.1, 0.2, 0.3],
+                          'model__max_depth': [3, 5, 7, 9],
+                          'model__subsample': [0.6, 0.8, 1.0],
+                          'model__colsample_bytree': [0.6, 0.8, 1.0]},
+    'AdaBoost Regressor': {'model__n_estimators': [50, 100, 150, 200],
+                           'model__learning_rate': [0.01, 0.1, 0.5, 1.0],
+                           'model__loss': ['linear', 'square', 'exponential']},
+    'Gradient Boosting Regressor': {
+        'model__n_estimators': [100, 200, 300, 400, 500],
+        'model__learning_rate': [0.01, 0.1, 0.2, 0.3],
+        'model__max_depth': [3, 5, 7, 9],
+        'model__min_samples_split': [2, 5, 10],
+        'model__min_samples_leaf': [1, 2, 4]}
 }
 
 # Sidebar - Trigger predictions
@@ -113,13 +190,38 @@ if st.sidebar.button('Predict Prices'):
     if target in df.columns and features:
         X = df[features]
         y = df[target]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            test_size=0.2,
+                                                            random_state=42)
         model = model_dict[model_type]
         pipeline = Pipeline([('preprocessor', preprocessor), ('model', model)])
-        pipeline.fit(X_train, y_train)
-        y_pred = pipeline.predict(X_test)
+
+        if search_type == "GridSearch":
+            search = GridSearchCV(pipeline, param_distributions[model_type],
+                                  cv=cv_folds, verbose=3)
+        elif search_type == "RandomizedSearch":
+            search = RandomizedSearchCV(pipeline,
+                                        param_distributions[model_type],
+                                        n_iter=n_iter_search, cv=cv_folds,
+                                        random_state=42, verbose=3)
+        else:
+            search = pipeline
+
+        # Progress bar
+        progress_bar = st.progress(0)
+        status_text = st.empty()
+        start_time = time()
+
+        search.fit(X_train, y_train)
+
+        progress_bar.progress(100)
+        status_text.text("Training complete!")
+
+        end_time = time()
 
         # Model performance metrics
+        y_pred = search.predict(X_test)
+
         st.header('Model Performance')
         mse = mean_squared_error(y_test, y_pred)
         mae = mean_absolute_error(y_test, y_pred)
@@ -130,9 +232,19 @@ if st.sidebar.button('Predict Prices'):
         st.write('---')
         st.write(f'Predicted mean price: {np.mean(y_pred)}')
 
+        # Best hyperparameters
+        if search_type in ["GridSearch", "RandomizedSearch"]:
+            st.write("Best Hyperparameters:")
+            st.write(search.best_params_)
+
         # Visualization of Actual vs Predicted
-        fig = px.scatter(x=y_test, y=y_pred, labels={'x': 'Actual', 'y': 'Predicted'}, title='Actual vs Predicted')
-        fig.add_trace(go.Scatter(x=[y_test.min(), y_test.max()], y=[y_test.min(), y_test.max()], mode='lines', line=dict(color='red', dash='dash'), name='Ideal Fit'))
+        fig = px.scatter(x=y_test, y=y_pred,
+                         labels={'x': 'Actual', 'y': 'Predicted'},
+                         title='Actual vs Predicted')
+        fig.add_trace(go.Scatter(x=[y_test.min(), y_test.max()],
+                                 y=[y_test.min(), y_test.max()], mode='lines',
+                                 line=dict(color='red', dash='dash'),
+                                 name='Ideal Fit'))
         st.plotly_chart(fig)
 
         # Feature importance or model coefficients visualization
@@ -141,20 +253,26 @@ if st.sidebar.button('Predict Prices'):
         elif hasattr(model, 'coef_'):
             importance = model.coef_
         else:
-            importance = [0]*len(features)  # Default to zeros if no importance or coefficients
+            importance = [0] * len(
+                features)  # Default to zeros if no importance or coefficients
 
         features = X_train.columns
-        fig_importance = px.bar(x=features, y=importance, labels={'x': 'Features', 'y': 'Importance or Weight'}, title='Feature Importance or Weights')
+        fig_importance = px.bar(x=features, y=importance,
+                                labels={'x': 'Features',
+                                        'y': 'Importance or Weight'},
+                                title='Feature Importance or Weights')
         st.plotly_chart(fig_importance)
 
         # Feature importance table with download option
-        feature_importance_df = pd.DataFrame({
-            'Features': features,
-            'Importance': importance
-        }).sort_values(by='Importance', ascending=False)
+        feature_importance_df = pd.DataFrame(
+            {'Features': features, 'Importance': importance}).sort_values(
+            by='Importance', ascending=False)
         st.write("Feature Importances")
         st.dataframe(feature_importance_df)
         csv = feature_importance_df.to_csv().encode('utf-8')
-        st.download_button(label="Download Feature Importances", data=csv, file_name='feature_importances.csv', mime='text/csv')
+        st.download_button(label="Download Feature Importances", data=csv,
+                           file_name='feature_importances.csv',
+                           mime='text/csv')
     else:
-        st.error("Please ensure the dataset includes the target column and select at least one feature.")
+        st.error(
+            "Please ensure the dataset includes the target column and select at least one feature.")
